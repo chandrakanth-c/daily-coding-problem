ca3fb985ac158e589dd5184ee405580b8ebc39c1
chandrakanth-c <65267759+chandrakanth-c@users.noreply.github.com>
2023-10-30 20:38:57 -0700

    Update dcp_4.java

--- dcp_4.java
+++ dcp_4.java
@@ -1,8 +1,9 @@
 /******************************************************************************
+Given an array of integers, find the first missing positive integer in linear time and constant space. 
+In other words, find the lowest positive integer that does not exist in the array. 
+The array can contain duplicates and negative numbers as well.
 
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
 
 *******************************************************************************/
 
